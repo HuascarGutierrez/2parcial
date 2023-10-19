09ab0a8a7042252b6c561646b0fdd796929fc7b1
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-19 03:24:34 -1200

    first item

---
+++ src/components/Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Item() {
+    const hola=''
+  return (
+    <div>
+        <img className='itemD'></img>
+    </div>
+  )
+}
+
+export default Item
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './user.css'
+function User() {
+  return (
+    <div className='back'>
+      <img className='photo' src='./avatar.svg'></img>
+    </div>
+  )
+}
+
+export default User
